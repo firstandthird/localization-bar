560076e91c2ec363b639fa41640b6625ac79060e
Michael Turnwall <freelance@turnwall.net>
2017-01-03 11:03:03 -0800

    Update so no HTML is needed on the page and documentation

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # Localization Bar
 
 Detect a user's browser language and show a message for the user to view pages in their preferred language.
+
+## Usage
+
+Import the class into your javascript. You'll then need to pass an object containing the options for the languages you want to use. Each language needs the following options.
+
+* message - the copy shown in the bar
+* cta - an object that contains
+    * text - text displayed for the cta
+    * url - url to the translated pages
+    
+Here is an example of what the object would look like.
+
+```javascript
+const languageMap = {
+    es: {
+      message: 'Ver el sitio en',
+        cta: {
+          text: 'español',
+          url: '/es'
+      }
+  },
+  fr: {
+      message: 'Voir le site en',
+      cta: {
+          text: 'français',
+          url: '/fr'
+      }
+  }
+};
+```
+
+By default the localization-bar will be appended to the top of the body element. If you would rather insert the bar into another part of the page, simply pass the selector of the element you want to insert before.
+
+When you are ready to add the bar to the page call the `check()` method.
+
+Example:
+
+```javascript
+const localizationBar = new LanguageBar({
+    es: {
+        message: 'Ver el sitio en',
+        cta: {
+            text: 'español',
+            url: '/es'
+        }
+    },
+    fr: {
+        message: 'Voir le site en',
+        cta: {
+            text: 'français',
+            url: '/fr'
+        }
+    }
+}, '.wrapper');
+localizationBar.check();
+```
--- locailization-bar.js
+++ locailization-bar.js
@@ -1,25 +1,13 @@
-// here is a version that is a es6 class
-class LanguageBar {
-  constructor(languageMap) {
+export default class {
+  constructor(languageMap, insertSelector = 'body') {
     this.languageMap = languageMap;
-    this.wrapper = document.querySelector('.language-bar');
-    this.language = this.findLanguage();
-    this.addCopy();
-    this.createCloseBtn();
+    this.insertSelector = insertSelector;
   }
-  createCloseBtn() {
-    const button = document.createElement('button');
-    button.classList.add('language-bar__close');
-    button.innerText = 'X';
-    button.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      this.close();
-    });
-    this.wrapper.appendChild(button);
-  }
-  close() {
-    this.wrapper.style.marginTop = `-${this.wrapper.offsetHeight}px`;
-  }
+
+  /**
+   * See if a browser preferred language matches a language option
+   * @returns {Object | Boolean} specific language options or false if no language is found
+   */
   findLanguage() {
     const selectedLanguages = navigator.languages || [navigator.userLanguage];
     for (let i = 0; i < selectedLanguages.length; i += 1) {
@@ -28,59 +16,60 @@ class LanguageBar {
         return this.languageMap[selectedLanguage];
       }
     }
-    return this.languageMap['en-us'];
+    return false;
   }
+
+  /**
+   * Check to see the localization bar is needed. If it is create the HTML and add to page
+   */
+  check() {
+    this.language = this.findLanguage();
+    if (this.language) {
+      this.addHtml();
+    }
+  }
+
+  /**
+   * Create all the HTML for the localization bar
+   */
+  addHtml() {
+    this.createWrapper();
+    this.addCopy();
+    this.createCloseBtn();
+  }
+
+  createWrapper() {
+    const bar = document.createElement('div');
+    bar.classList.add('localization-bar');
+    if (this.insertSelector === 'body') {
+      this.wrapper = document.body.insertBefore(bar, document.body.firstChild);
+    } else {
+      const insertionPoint = document.querySelector(this.insertSelector);
+      this.wrapper = insertionPoint.insertBefore(bar, insertionPoint.firstChild);
+    }
+  }
+
+  createCloseBtn() {
+    const button = document.createElement('button');
+    button.classList.add('localization-bar__close');
+    button.innerText = 'X';
+    button.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      this.close();
+    });
+    this.wrapper.appendChild(button);
+  }
+
+  close() {
+    this.wrapper.style.marginTop = `-${this.wrapper.offsetHeight}px`;
+  }
+
   addCopy() {
     const languageCta = document.createElement('a');
-    languageCta.classList.add('language-bar__cta');
+    languageCta.classList.add('localization-bar__cta');
     languageCta.href = this.language.cta.url;
     languageCta.innerText = this.language.cta.text;
     this.wrapper.innerText = `${this.language.message} `;
     this.wrapper.appendChild(languageCta);
   }
 }
-
-// also a version that can be an export
-// const languageBar = (languageMap) => {
-//   const selectedLanguages = navigator.languages || [navigator.userLanguage];
-//   let language = '';
-//   for (let i = 0; i < selectedLanguages.length; i += 1) {
-//     const selectedLanguage = selectedLanguages[i].toLowerCase();
-//     if (selectedLanguage in languageMap) {
-//       language = languageMap[selectedLanguage];
-//       break;
-//     }
-//   }
-//   const languageBar = document.querySelector('.language-bar');
-//   const languageCta = document.createElement('a');
-//   languageCta.classList.add('language-bar__cta');
-//   languageCta.href = language.cta.url;
-//   languageCta.innerText = language.cta.text;
-//   languageBar.innerText = `${language.message} `;
-//   languageBar.appendChild(languageCta);
-// };
-const languageMap = {
-  'en-us': {
-    message: 'View the site in',
-    cta: {
-      text: 'english',
-      url: '/en'
-    }
-  },
-  es: {
-    message: 'Ver el sitio en',
-    cta: {
-      text: 'español',
-      url: '/es'
-    }
-  },
-  fr: {
-    message: 'Voir le site en',
-    cta: {
-      text: 'français',
-      url: '/fr'
-    }
-  },
-};
-// languageBar(languageMap);
-new LanguageBar(languageMap);
